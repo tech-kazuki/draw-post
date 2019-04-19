219e83234d3979101b96bbdedf9d9d66251aa216
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-19 18:19:28 +0900

    Edit file

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -30,7 +30,6 @@ $(document).on('turbolinks:load', function() {
             var rect = e.target.getBoundingClientRect();
             var X = ~~(e.clientX - rect.left);
             var Y = ~~(e.clientY - rect.top);
-            //draw 関数にマウスの位置を渡す
             draw(X, Y);
         };
     };
@@ -40,7 +39,6 @@ $(document).on('turbolinks:load', function() {
             var rect = e.target.getBoundingClientRect();
             var X = ~~(e.clientX - rect.left);
             var Y = ~~(e.clientY - rect.top);
-            //draw 関数にマウスの位置を渡す
             draw(X, Y);
         }
     };
@@ -48,22 +46,18 @@ $(document).on('turbolinks:load', function() {
     function draw(X, Y) {
         ctx.beginPath();
         ctx.globalAlpha = defoalpha;
-        //マウス継続値によって場合分け、直線の moveTo（スタート地点）を決定
         if (mouseX === "") {
-            //継続値が初期値の場合は、現在のマウス位置をスタート位置とする
             ctx.moveTo(X, Y);
         } else {
-            //継続値が初期値ではない場合は、前回のゴール位置を次のスタート位置とする
             ctx.moveTo(mouseX, mouseY);
         }
-        //lineTo（ゴール地点）の決定、現在のマウス位置をゴール地点とする
+
         ctx.lineTo(X, Y);
-        //直線の角を「丸」、サイズと色を決める
+
         ctx.lineCap = "round";
         ctx.lineWidth = defosize * 2;
         ctx.strokeStyle = defocolor;
         ctx.stroke();
-        //マウス継続値に現在のマウス位置、つまりゴール位置を代入
         mouseX = X;
         mouseY = Y;
     };
@@ -85,7 +79,6 @@ $(document).on('turbolinks:load', function() {
 
     //メニューボタン管理
     function canvasMenu() {
-        //id 値によって場合分け
         var thisId = this.id;
         if (thisId.indexOf("size") + 1) {
             defosize = this.id.slice(4);
@@ -101,7 +94,6 @@ $(document).on('turbolinks:load', function() {
             ctx.fillRect(0, 0, 700, 500);
         }
         if (thisId.indexOf("clear") + 1) {
-            //全消しボタン、OKされた場合は fillRect 長方形で覆います
             if (confirm("すべて消去しますか？")) {
                 ctx.beginPath();
                 ctx.fillStyle = "#fff";
